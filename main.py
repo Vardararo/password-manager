3be54a880bdc1896f9cef6362fa24b6d95a4cf18
Edib H <edib.hafizovic@gmail.com>
2024-06-24 20:00:22 +0200

    add display_stored_data function

--- main.py
+++ main.py
@@ -43,8 +43,10 @@ def save():
             "password": password,
     }}
     
+    # Check if the correct credentials are entered
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Error", message="Please don't leave empty fields!")
+    # Review inserted data before saving to a file
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                       f"\nPassword: {password} \nIs it ok to save?")  
@@ -64,8 +66,8 @@ def save():
                 website_entry.delete(0, END)
                 pw_entry.delete(0, END)
 
+# Find a password for given website
 def find_password():
-    
     website = website_entry.get()
     try:
         with open("data.json") as file:
@@ -79,10 +81,18 @@ def find_password():
             messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
         else:
             messagebox.showerror(title="Error", message=f"No details exist for {website}!")
-#TODO - function to display generated data
+
+# Display all the passwords saved to the data.json file
 def display_stored_data():
-    
-    pass
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            stored_data_text.delete(1.0, END)
+            for website, details in data.items():
+                stored_data_text.insert(END, f"Website: {website}\nEmail: {details['email']}\nPassword: {details['password']}\n\n")
+    except FileNotFoundError:
+        stored_data_text.delete(1.0, END)
+        stored_data_text.insert(END, "No stored data found.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -133,9 +143,9 @@ add_btn.grid(column=1,row=4, columnspan=2, sticky="EW")
 search_btn = Button(tab1, text="Search", command=find_password)
 search_btn.grid(column=2, row=1, sticky="EW")
 
+
 # Tab 2 UI
 # Display stored data
-
 stored_data_text = Text(tab2, width=60, height=20)
 stored_data_text.grid(column=0, row=0)
 display_btn = Button(tab2, text="Refresh Data", command=display_stored_data)
