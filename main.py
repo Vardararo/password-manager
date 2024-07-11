3aaa714acc530fae5589ce46930246996db00f87
Edib H <edib.hafizovic@gmail.com>
2024-07-11 12:39:14 +0200

    Use string module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# from tkinter import messagebox, ttk
+from string import ascii_letters, punctuation
 import random
 import pyperclip
 import json
@@ -13,9 +13,9 @@ from PIL import Image
 def generate_password():
     pw_entry.delete(0, END)
     
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = list(ascii_letters)
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    symbols = list(punctuation)
 
     nr_letters = random.randint(6, 8)
     nr_symbols = random.randint(2, 4)
